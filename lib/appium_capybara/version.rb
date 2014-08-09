bdd66a2dc3a10fa2f8211b61d3b4ea8ef1315155
bootstraponline <code@bootstraponline.com>
2014-08-08 21:02:07 -0400

    Release 0.1.0

--- lib/appium_capybara/version.rb
+++ lib/appium_capybara/version.rb
@@ -1,6 +1,6 @@
 module Appium
   module Capybara
-    VERSION = '0.0.4' unless defined? ::Appium::Capybara::VERSION
-    DATE    = '2014-08-07' unless defined? ::Appium::Capybara::DATE
+    VERSION = '0.1.0' unless defined? ::Appium::Capybara::VERSION
+    DATE    = '2014-08-08' unless defined? ::Appium::Capybara::DATE
   end
 end
