1ab856478e7495347dbbd5d2737c9cc55106b3bf
Scott Bonebrake <sbonebrake@yammer-inc.com>
2014-12-29 13:58:51 -0800

    Release 1.2.1

--- lib/appium_capybara/version.rb
+++ lib/appium_capybara/version.rb
@@ -1,6 +1,6 @@
 module Appium
   module Capybara
-    VERSION = '1.2.0' unless defined? ::Appium::Capybara::VERSION
-    DATE    = '2014-11-24' unless defined? ::Appium::Capybara::DATE
+    VERSION = '1.2.1' unless defined? ::Appium::Capybara::VERSION
+    DATE    = '2014-12-29' unless defined? ::Appium::Capybara::DATE
   end
 end
