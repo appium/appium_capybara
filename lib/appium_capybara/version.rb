99963c23e11081f53e82dabd9336a296f1bd5a0a
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-05-08 19:54:12 +0900

    Release 1.5.0

--- lib/appium_capybara/version.rb
+++ lib/appium_capybara/version.rb
@@ -1,6 +1,6 @@
 module Appium
   module Capybara
-    VERSION = '1.4.1' unless defined? ::Appium::Capybara::VERSION
-    DATE    = '2017-10-29' unless defined? ::Appium::Capybara::DATE
+    VERSION = '1.5.0' unless defined? ::Appium::Capybara::VERSION
+    DATE    = '2018-05-08' unless defined? ::Appium::Capybara::DATE
   end
 end
