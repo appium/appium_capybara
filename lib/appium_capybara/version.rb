503a8d5bfbc058257a4352fb68a841b1c4628f62
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2021-11-11 01:10:50 -0800

    chore: bump version

--- lib/appium_capybara/version.rb
+++ lib/appium_capybara/version.rb
@@ -1,6 +1,6 @@
 module Appium
   module Capybara
-    VERSION = '2.0.0.rc2'.freeze unless defined? ::Appium::Capybara::VERSION
-    DATE    = '2021-11-05'.freeze unless defined? ::Appium::Capybara::DATE
+    VERSION = '2.0.0'.freeze unless defined? ::Appium::Capybara::VERSION
+    DATE    = '2021-11-11'.freeze unless defined? ::Appium::Capybara::DATE
   end
 end
