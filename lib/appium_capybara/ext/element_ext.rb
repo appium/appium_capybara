4072558be376c69cb4e7b84e21967c0957403a56
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-11-12 22:38:29 -0800

    fix: add webdrivererror handler instead of calling just name (#66)

--- lib/appium_capybara/ext/element_ext.rb
+++ lib/appium_capybara/ext/element_ext.rb
@@ -1,6 +1,10 @@
 class Capybara::Node::Element
   # Override
   def inspect
-    %(#<Capybara::Node::Element name=#{@base.name}>)
+    %(#<#{self.class} tag="#{tag_name}" path="#{path}">)
+  rescue ::Capybara::NotSupportedByDriverError, ::Selenium::WebDriver::Error::WebDriverError
+    # Native context does not have the 'path' strategy, so it could raise an exception.
+    # Then, the method can call tag_name instead.
+    %(#<#{self.class} tag="#{tag_name}">)
   end
 end
