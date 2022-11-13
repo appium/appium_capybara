5704c968c4f205898020d4496f65e971ac808c11
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-11-12 23:14:56 -0800

    Release 2.0.1

--- lib/appium_capybara/version.rb
+++ lib/appium_capybara/version.rb
@@ -1,6 +1,6 @@
 module Appium
   module Capybara
-    VERSION = '2.0.0'.freeze unless defined? ::Appium::Capybara::VERSION
-    DATE    = '2021-11-11'.freeze unless defined? ::Appium::Capybara::DATE
+    VERSION = '2.0.1'.freeze unless defined? ::Appium::Capybara::VERSION
+    DATE    = '2022-11-12'.freeze unless defined? ::Appium::Capybara::DATE
   end
 end
