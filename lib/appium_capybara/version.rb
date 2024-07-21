b28239336dbd2ba0c2ad5b5089b4c9666485b589
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-07-20 22:51:38 -0700

    Release 3.1.1

--- lib/appium_capybara/version.rb
+++ lib/appium_capybara/version.rb
@@ -1,6 +1,6 @@
 module Appium
   module Capybara
-    VERSION = '3.1.0'.freeze unless defined? ::Appium::Capybara::VERSION
+    VERSION = '3.1.1'.freeze unless defined? ::Appium::Capybara::VERSION
     DATE    = '2024-07-20'.freeze unless defined? ::Appium::Capybara::DATE
   end
 end
